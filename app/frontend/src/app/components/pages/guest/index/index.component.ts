f9150d0d12ba3f37e21d086c4f70c8ea4be8db4b
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2024-02-14 14:59:42 +0100

    Searching and sorting on guest index page implemented.

--- app/frontend/src/app/components/pages/guest/index/index.component.ts
+++ app/frontend/src/app/components/pages/guest/index/index.component.ts
@@ -15,6 +15,31 @@ export class IndexComponent implements OnInit {
   numberOfTeachers: string = ""
   data: Data = new Data()
   allTeachers: User[] = []
+  shouldShowSubjects: boolean = false
+
+  subjectsSorted: string[] = []
+  allTeachersSorted: User[] = []
+
+  shouldShowSortedSubjects: boolean = false
+  shouldSortSubjectsBySubject: boolean = false
+  shouldSortSubjectsByTeacherName: boolean = false
+  shouldSortSubjectsByTeacherSurname: boolean = false
+
+  shouldShowSearchResults: boolean = false
+  searchTeacherName: string = ""
+  searchTeacherSurname: string = ""
+  searchSubject: string = ""
+
+  subjectsSearched: string[] = []
+  allTeachersSearched: User[] = []
+
+  subjectsSearchedSorted: string[] = []
+  allTeachersSearchedSorted: User[] = []
+
+  shouldShowSortedTeachers: boolean = false
+  shouldSortTeachersByTeacherName: boolean = false
+  shouldSortTeachersByTeacherSurname: boolean = false
+  shouldSortTeachersBySubject: boolean = false
 
   constructor(
     private defaultService: DefaultService,
@@ -42,5 +67,124 @@ export class IndexComponent implements OnInit {
         )
       }
     )
+  }
+
+  sortSubjects() {
+    if (this.shouldSortSubjectsByTeacherName || this.shouldSortSubjectsByTeacherSurname || this.shouldSortSubjectsBySubject) {
+      this.shouldShowSortedSubjects = true
+    }
+    else {
+      this.shouldShowSortedSubjects = false
+      return
+    }
+    this.subjectsSorted = []
+    this.allTeachersSorted = []
+    this.data.subjects.forEach((subject) => this.subjectsSorted.push(subject))
+    this.allTeachers.forEach((teacher) => this.allTeachersSorted.push(teacher))
+    if (this.shouldSortSubjectsBySubject) {
+      this.subjectsSorted = this.subjectsSorted.sort((subject1, subject2) => subject1.localeCompare(subject2))
+    }
+    if (this.shouldSortSubjectsByTeacherName && !this.shouldSortSubjectsByTeacherSurname) {
+      this.allTeachersSorted = this.allTeachersSorted.sort((teacher1, teacher2) => teacher1.name.localeCompare(teacher2.name))
+    }
+    if (!this.shouldSortSubjectsByTeacherName && this.shouldSortSubjectsByTeacherSurname) {
+      this.allTeachersSorted = this.allTeachersSorted.sort((teacher1, teacher2) => teacher1.surname.localeCompare(teacher2.surname))
+    }
+    if (this.shouldSortSubjectsByTeacherName && this.shouldSortSubjectsByTeacherSurname) {
+      this.allTeachersSorted = this.allTeachersSorted.sort((teacher1, teacher2) => {
+        const nameComparisonResult = teacher1.name.localeCompare(teacher2.name)
+        if (nameComparisonResult == 0) {
+          return teacher1.surname.localeCompare(teacher2.surname)
+        } else {
+          return nameComparisonResult
+        }
+      })
+    }
+  }
+
+  sortTeachers() {
+    if (this.shouldSortTeachersByTeacherName || this.shouldSortTeachersByTeacherSurname || this.shouldSortTeachersBySubject) {
+      this.shouldShowSortedTeachers = true
+    }
+    else {
+      this.shouldShowSortedTeachers = false
+      return
+    }
+    this.subjectsSearchedSorted = []
+    this.allTeachersSearchedSorted = []
+    this.data.subjects.forEach((subject) => this.subjectsSearchedSorted.push(subject))
+    this.allTeachersSearched.forEach((teacher) => this.allTeachersSearchedSorted.push(teacher))
+    if (this.shouldSortTeachersByTeacherName && !this.shouldSortTeachersByTeacherSurname) {
+      this.allTeachersSearchedSorted = this.allTeachersSearchedSorted.sort((teacher1, teacher2) => teacher1.name.localeCompare(teacher2.name))
+    }
+    if (!this.shouldSortTeachersByTeacherName && this.shouldSortTeachersByTeacherSurname) {
+      this.allTeachersSearchedSorted = this.allTeachersSearchedSorted.sort((teacher1, teacher2) => teacher1.surname.localeCompare(teacher2.surname))
+    }
+    if (this.shouldSortTeachersByTeacherName && this.shouldSortTeachersByTeacherSurname) {
+      this.allTeachersSearchedSorted = this.allTeachersSearchedSorted.sort((teacher1, teacher2) => {
+        const nameComparisonResult = teacher1.name.localeCompare(teacher2.name)
+        if (nameComparisonResult == 0) {
+          return teacher1.surname.localeCompare(teacher2.surname)
+        } else {
+          return nameComparisonResult
+        }
+      })
+    }
+    if (this.shouldSortTeachersBySubject) {
+      this.subjectsSearchedSorted = this.subjectsSearchedSorted.sort((subject1, subject2) => subject1.localeCompare(subject2))
+    }
+
+  }
+
+  search() {
+    this.subjectsSearched = []
+    this.allTeachersSearched = []
+    this.data.subjects.forEach((subject) => this.subjectsSearched.push(subject))
+    this.allTeachers.forEach((teacher) => this.allTeachersSearched.push(teacher))
+    if (this.searchTeacherName != "") {
+      this.allTeachersSearched = this.allTeachersSearched.filter(
+        (teacher) => teacher.name.toLocaleLowerCase().includes(this.searchTeacherName.toLocaleLowerCase())
+      )
+    }
+    if (this.searchTeacherSurname != "") {
+      this.allTeachersSearched = this.allTeachersSearched.filter(
+        (teacher) => teacher.surname.toLocaleLowerCase().includes(this.searchTeacherSurname.toLocaleLowerCase())
+      )
+    }
+    if (this.searchSubject != "") {
+      this.allTeachersSearched = this.allTeachersSearched.filter((teacher) => {
+        let foundSubject = false
+        teacher.teacherSubjects.forEach(
+          (subject) => {
+            if (subject.toLocaleLowerCase().includes(this.searchSubject.toLocaleLowerCase())) {
+              foundSubject = true
+            }
+          }
+        )
+        return foundSubject
+      })
+    }
+  }
+
+  showSubjects() {
+    this.shouldShowSubjects = true
+  }
+
+  hideSubjects() {
+    this.shouldShowSubjects = false
+    this.shouldShowSortedSubjects = false
+    this.shouldSortSubjectsByTeacherName = this.shouldSortSubjectsByTeacherSurname = this.shouldSortSubjectsBySubject = false
+
+  }
+
+  showSearchResults() {
+    this.search()
+    this.shouldShowSearchResults = true
+  }
+
+  hideSearchResults() {
+    this.shouldShowSearchResults = false
+    this.shouldShowSortedTeachers = false
+    this.shouldSortTeachersByTeacherName = this.shouldSortTeachersByTeacherSurname = this.shouldSortTeachersBySubject = false
   }
 }
