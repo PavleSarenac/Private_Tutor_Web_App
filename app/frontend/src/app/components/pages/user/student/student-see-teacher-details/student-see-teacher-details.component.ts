8ecd6436ae468011fa422dcc9e048dc63d034599
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2024-02-15 02:22:02 +0100

    Frontend for student form for scheduling a class implemented.

--- app/frontend/src/app/components/pages/user/student/student-see-teacher-details/student-see-teacher-details.component.ts
+++ app/frontend/src/app/components/pages/user/student/student-see-teacher-details/student-see-teacher-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Class } from 'src/app/models/class.model';
 import { User } from 'src/app/models/user.model';
 import { DefaultService } from 'src/app/services/default/default.service';
 
@@ -11,12 +12,15 @@ export class StudentSeeTeacherDetailsComponent implements OnInit {
   teacher: User = new User()
   profilePictureUrl: any = null
 
+  class: Class = new Class()
+
   constructor(private defaultService: DefaultService) { }
 
   ngOnInit(): void {
     this.defaultService.getUser(localStorage.getItem("studentSeeTeacherDetailsUsername")!).subscribe(
       (teacher) => {
         this.teacher = teacher
+        this.initializeClass()
         this.defaultService.getProfilePicture(this.teacher.profilePicturePath).subscribe(
           (profilePictureFile: Blob) => {
             const fileReader = new FileReader()
@@ -33,5 +37,15 @@ export class StudentSeeTeacherDetailsComponent implements OnInit {
         )
       }
     )
+  }
+
+  initializeClass() {
+    this.class.teacher = this.teacher.username
+    this.class.student = JSON.parse(localStorage.getItem("loggedInUser")!).username
+    if (this.teacher.teacherSubjects.length == 1) this.class.subject = this.teacher.teacherSubjects[0]
+  }
+
+  updateChosenSubject(subject: string) {
+    this.class.subject = subject
   }
 }
---
+++ app/frontend/src/app/models/class.model.ts
@@ -0,0 +1,9 @@
+export class Class {
+  student: string = ""
+  teacher: string = ""
+  subject: string = ""
+  date: string = ""
+  time: string = ""
+  description: string = ""
+  isDoubleClass: boolean = false
+}
